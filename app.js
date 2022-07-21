0a82d3723ca5105900c2e2698daebaf36fbd2403
abdullahalam123 <abdullah.alam.tapadar2000@gmail.com>
2022-07-21 16:10:47 +0530

    add gitignore, procfile

--- app.js
+++ app.js
@@ -163,6 +163,11 @@ app.post("/login", function(req, res){
   });
 });
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
   console.log("server started successfully");
 });
